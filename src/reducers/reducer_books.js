c3da9ee1a9546c7e2092a83bb0e3637e46819505
Unknown <dumn3z0@gmail.com>
2018-01-28 21:03:17 +0200

    book-app final commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function() {
+    return [
+        { title: 'Maica Domnului "Semnul"', pages: 100 },
+        { title: 'Sa nu ucizi o pasare cantatoare...', pages: 45 },
+        { title: 'Maytrei', pages: 121 },
+        { title: 'Once Upon A time In The West', pages: 43 }
+    ]
+}
